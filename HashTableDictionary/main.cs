3987eb238f31a4fcb13cf84a98f9f20b98c087e3
Borja Fernández Gauna <borjafdezgauna>
2024-09-04 12:50:15 +0200

    Removed api-related stuff

--- HashTableDictionary/main.cs
+++ HashTableDictionary/main.cs
@@ -5,18 +5,8 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        StatsUpdater statsUpdater = new StatsUpdater();
-        statsUpdater.CheckGitUsername();
-
-        if (statsUpdater.GitUsername == null)
-        {
-            Console.WriteLine("Error getting Git info. You need to add this project to Git (clone from my repo, delete the .git directory and create a new repo on GitHub)");
-            Console.WriteLine("If you have already done that, run Visual Studio Installer and check you have installed \"Git for Windows\"");
-            return;
-        }
-
         HashTableDictionary<int, string> dictionaryIntString = new HashTableDictionary<int, string>();
-        Console.WriteLine("Testing BinaryTreeDictionary<int,string>:\n");
+        Console.WriteLine("Testing HashTableDictionary<int,string>:\n");
         bool success = Common.Tests.TestDictionaryIntString(dictionaryIntString);
         if (!success)
             return;
@@ -27,11 +17,6 @@ class MainClass
             Console.WriteLine("An error was detected during the speed measurement. Probably something wrong with Add/Get");
             return;
         }
-
-        success = statsUpdater.AddResult(speedMeasure);
-        if (success)
-            Console.WriteLine("Your time was saved on the server");
-        else
-            Console.WriteLine("Something failed saving your time on the server");
+        Console.WriteLine($"All tests passed. Time: {speedMeasure.Time}s");
     }
 }
